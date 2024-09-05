89ea2fa5318cd1d39449e2fc420fba0829e0ccbd
Diego Gaspar <cadastro@diegogaspar.dev.br>
2024-09-04 22:54:53 -0300

    fix

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function VerificationTokenFail() {
+  return (
+    <div>
+      <h1>SIGN-IN</h1>
+    </div>
+  );
+}
---
+++ src/app/verification-token-fail/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function VerificationTokenFail() {
+  return (
+    <div>
+      <h1>Falha ao verificar usuário!</h1>
+    </div>
+  );
+}
